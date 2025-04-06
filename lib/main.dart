6e05fe152856a1f477a169ef3fe62f5ccd31ddcb
8ahmadreza3 <8arhp3@gmail.com>
2025-04-06 16:24:56 +0330

    my app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// Main application widget
+// A widget that does not require mutable state (immutable UI elements).
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Main app configuration with light blue theme, custom app bar and FAB styling
+    
+}
